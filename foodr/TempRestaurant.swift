afc7a63f1b2f78b8794fd0f86446c3a84c8713a3
Phong Bach <phong.bach@wsu.edu>
2021-04-19 17:34:28 -0700

    Add temp restaurant class

---
+++ foodr/TempRestaurant.swift
@@ -0,0 +1,37 @@
+//
+//  TempRestaurant.swift
+//  foodr
+//
+//  Created by Phong Bach on 4/19/21.
+//
+
+import UIKit
+
+class TempRestaurant {
+    var name: String
+    var address: String
+    var city: String
+    var state: String
+    var rating: Float
+    var id: String
+    var image: Data
+    var note: String?
+    
+    init(name: String,
+         id: String,
+         address: String,
+         city: String,
+         state: String,
+         rating: Float,
+         image: Data,
+         note: String = "") {
+        self.name = name
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.rating = rating
+        self.image = image
+        self.note = note
+    }
+}
